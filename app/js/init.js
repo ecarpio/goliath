ae9f8eeb790cd2da5adf4878fae8e3f9d6a8c786
Ed <carpio.ed@gmail.com>
2020-11-16 17:02:41 -0800

    fixded table data

--- app/js/init.js
+++ app/js/init.js
@@ -1,5 +1,12 @@
 $(function(){
     $('#example').DataTable({
-        "ajax": '/js/sampletable.txt'
+        "ajax": '/js/sampletable.txt',
+        "columns": [
+            { "data": "name" },
+            { "data": "email" },
+            { "data": "phone" },
+            { "data": "date-addded" },
+            { "data": "prequal" }
+        ]
     });
 })
